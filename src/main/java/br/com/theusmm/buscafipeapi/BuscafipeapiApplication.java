5d5386a448c8de5dd3963354c03d2fcb3ec03ddf
theusmm <theusmm75@gmail.com>
2024-03-05 19:01:05 -0300

    Inicio do projeto Api FIPE HTTP Rest

---
+++ src/main/java/br/com/theusmm/buscafipeapi/BuscafipeapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.theusmm.buscafipeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscafipeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscafipeapiApplication.class, args);
+	}
+
+}
